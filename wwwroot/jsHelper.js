8626bdc2f42b48c498d99639edf3c723c9030975
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2019-06-18 22:58:57 +0200

    Add project files.

---
+++ wwwroot/jsHelper.js
@@ -0,0 +1,20 @@
+﻿function alertInterop(text) {
+    alert(text);
+}
+
+function focusInterop(id) {
+    document.getElementById(id).focus();
+    return true;
+}
+
+function preventScrolling(id) {
+    const element = document.getElementById(id);
+
+    window.addEventListener("keydown", function (e) {
+        // space and arrow keys
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+}
+
